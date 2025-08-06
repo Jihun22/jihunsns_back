096a669c3873fcf7f44b894c8baa50cfb3eca646
YangJihun <ik5334@naver.com>
2025-08-06 18:32:23 +0900

    Initial commit

---
+++ src/main/java/com/jihunsns_back/JihunsnsBackApplication.java
@@ -0,0 +1,13 @@
+package com.jihunsns_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JihunsnsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JihunsnsBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihunsns_back/JihunsnsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihunsns_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JihunsnsBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
