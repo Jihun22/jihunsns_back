fcacfe4eb2b032f27c00003a3e4ba4dfc6251782
YangJihun <ik5334@naver.com>
2025-08-11 18:22:15 +0900

    feat: Test

---
+++ src/test/java/com/jihunsns_back/domain/entity/PostTest.java
@@ -0,0 +1,40 @@
+package com.jihunsns_back.domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setContent() {
+    }
+
+    @Test
+    void setCreatedAt() {
+    }
+
+    @Test
+    void setAuthor() {
+    }
+}
---
+++ src/test/java/com/jihunsns_back/domain/entity/UserTest.java
@@ -0,0 +1,56 @@
+package com.jihunsns_back.domain.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getNickname() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getRole() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void setEmail() {
+    }
+
+    @Test
+    void setNickname() {
+    }
+
+    @Test
+    void setPassword() {
+    }
+
+    @Test
+    void setRole() {
+    }
+
+    @Test
+    void setCreatedAt() {
+    }
+}
